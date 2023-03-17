5c715ec2c935df38f1786448bd8de7828d628039
gabrielSdejesus <gabrieldejesus9905@gmail.com>
2023-03-17 10:09:47 -0300

    wip

--- src/main/java/br/com/dbc/javamosdecolar/dto/CompradorDTO.java
+++ src/main/java/br/com/dbc/javamosdecolar/dto/CompradorDTO.java
@@ -14,6 +14,7 @@ import javax.persistence.Column;
 @NoArgsConstructor
 public class CompradorDTO extends CompradorCreateDTO {
 
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @Schema(description = "status do usuario", example = "true")
     private Boolean ativo;
 
