a3f9ef9cc605ba4c4ca4620a303c8219d68beb3c
Bruno <brunorodriguesdias@gmail.com>
2023-03-17 20:36:13 -0300

    Criando paginação de venda

--- src/main/java/br/com/dbc/javamosdecolar/controller/VendaController.java
+++ src/main/java/br/com/dbc/javamosdecolar/controller/VendaController.java
@@ -1,16 +1,23 @@
 package br.com.dbc.javamosdecolar.controller;
 
 import br.com.dbc.javamosdecolar.docs.VendaDoc;
+import br.com.dbc.javamosdecolar.dto.PageDTO;
 import br.com.dbc.javamosdecolar.exception.RegraDeNegocioException;
 import br.com.dbc.javamosdecolar.dto.VendaCreateDTO;
 import br.com.dbc.javamosdecolar.dto.VendaDTO;
 import br.com.dbc.javamosdecolar.service.VendaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.*;
@@ -22,17 +29,7 @@ import static org.springframework.http.HttpStatus.*;
 public class VendaController implements VendaDoc{
 
     private final VendaService vendaService;
-    @Override
-    @PostMapping
-    public ResponseEntity<VendaDTO> create(@RequestBody @Valid VendaCreateDTO vendaDTO) throws RegraDeNegocioException {
-        return new ResponseEntity<>(vendaService.create(vendaDTO), CREATED);
-    }
-    @Override
-    @DeleteMapping("/{idVenda}/cancelar")
-    public ResponseEntity<Void> delete(@PathVariable("idVenda") Integer idVenda) throws RegraDeNegocioException {
-        vendaService.delete(idVenda);
-        return ResponseEntity.noContent().build();
-    }
+
     @Override
     @GetMapping("/{idComprador}/comprador")
     public ResponseEntity<List<VendaDTO>> getByHistoricoCompras(@PathVariable("idComprador") Integer id)
@@ -44,5 +41,25 @@ public class VendaController implements VendaDoc{
     public ResponseEntity<List<VendaDTO>> getByHistoricoVendas(@PathVariable("idCompanhia") Integer id)
             throws RegraDeNegocioException {
         return new ResponseEntity<>(vendaService.getHistoricoVendasCompanhia(id), OK);
+    }
+    @GetMapping("/vendas-between")
+    public ResponseEntity<PageDTO<VendaDTO>> getVendasBetween(@RequestParam LocalDate inicioConsulta,
+                                                           @RequestParam LocalDate fimConsulta,
+                                                           @RequestParam Integer paginaSolicitada,
+                                                           @RequestParam Integer tamanhoPagina) {
+        return new ResponseEntity<>(vendaService.getVendasBetween(inicioConsulta,
+                fimConsulta, paginaSolicitada, tamanhoPagina), OK);
+    }
+    @Override
+    @PostMapping
+    public ResponseEntity<VendaDTO> create(@RequestBody @Valid VendaCreateDTO vendaDTO) throws RegraDeNegocioException {
+        return new ResponseEntity<>(vendaService.create(vendaDTO), CREATED);
+    }
+
+    @Override
+    @DeleteMapping("/{idVenda}/cancelar")
+    public ResponseEntity<Void> delete(@PathVariable("idVenda") Integer idVenda) throws RegraDeNegocioException {
+        vendaService.delete(idVenda);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/br/com/dbc/javamosdecolar/repository/VendaRepository.java
+++ src/main/java/br/com/dbc/javamosdecolar/repository/VendaRepository.java
@@ -3,15 +3,20 @@ package br.com.dbc.javamosdecolar.repository;
 import br.com.dbc.javamosdecolar.entity.CompanhiaEntity;
 import br.com.dbc.javamosdecolar.entity.CompradorEntity;
 import br.com.dbc.javamosdecolar.entity.VendaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface VendaRepository extends JpaRepository<VendaEntity, Integer> {
     List<VendaEntity> findAllByIdComprador(Integer idComprador);
+
+    Page<VendaEntity> findAllByDataBetween(Pageable pageable, LocalDate inicio, LocalDate fim);
 
     List<VendaEntity> findAllByIdCompanhia(Integer idCompanhia);
 }
