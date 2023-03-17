b65441a1184ee6f5db7b424919ca5cbabe50c313
Gabriel-Gomes-Meira <gabrielgomesm.contato@gmail.com>
2023-03-17 16:44:10 -0300

    Refactor BD

--- bd/dados.sql
+++ bd/dados.sql
@@ -56,61 +56,62 @@ INSERT INTO AVIACAO.COMPANHIA (cnpj, nome_fantasia, id_usuario)
 
 -- trechos --
 
-INSERT INTO AVIACAO.TRECHO (id_trecho, origem, destino, id_companhia)
-	VALUES (AVIACAO.seq_trecho.nextval, 'BEL', 'CWB', 1);
-INSERT INTO AVIACAO.TRECHO (id_trecho, origem, destino, id_companhia)
-	VALUES (AVIACAO.seq_trecho.nextval, 'POA', 'RJ', 2);
-INSERT INTO AVIACAO.TRECHO (id_trecho, origem, destino, id_companhia)
-	VALUES (AVIACAO.seq_trecho.nextval, 'SP', 'POA', 3);
-INSERT INTO AVIACAO.TRECHO (id_trecho, origem, destino, id_companhia)
-	VALUES (AVIACAO.seq_trecho.nextval, 'POA', 'FLO', 4);
-INSERT INTO AVIACAO.TRECHO (id_trecho, origem, destino, id_companhia)
-	VALUES (AVIACAO.seq_trecho.nextval, 'SP', 'RJ', 5);
+INSERT INTO AVIACAO.TRECHO (id_trecho, origem, destino)
+	VALUES (AVIACAO.seq_trecho.nextval, 'BEL', 'CWB');
+INSERT INTO AVIACAO.TRECHO (id_trecho, origem, destino)
+	VALUES (AVIACAO.seq_trecho.nextval, 'POA', 'RJ');
+INSERT INTO AVIACAO.TRECHO (id_trecho, origem, destino)
+	VALUES (AVIACAO.seq_trecho.nextval, 'SP', 'POA');
+INSERT INTO AVIACAO.TRECHO (id_trecho, origem, destino)
+	VALUES (AVIACAO.seq_trecho.nextval, 'POA', 'FLO');
+INSERT INTO AVIACAO.TRECHO (id_trecho, origem, destino)
+	VALUES (AVIACAO.seq_trecho.nextval, 'SP', 'RJ');
 
 -- vendas --
 
 INSERT INTO AVIACAO.VENDA  (id_venda, status, data, id_companhia, id_comprador, codigo)
-	VALUES (AVIACAO.seq_venda.nextval, 1, TIMESTAMP '2023-03-16 14:30:00', 1, 6, 'd3f84afb-30ba-40bf-a8e4-1fe43e29f81f');
+	VALUES (AVIACAO.seq_venda.nextval, '0', TIMESTAMP '2023-03-16 14:30:00', 1, 6, 'd3f84afb-30ba-40bf-a8e4-1fe43e29f81f');
 
 INSERT INTO AVIACAO.VENDA  (id_venda, status, data, id_companhia, id_comprador, codigo)
-	VALUES (AVIACAO.seq_venda.nextval, 1, TIMESTAMP '2023-03-16 14:30:00', 2, 7, 'b9e19222-412e-43eb-8f37-46ab322e77d3');
+	VALUES (AVIACAO.seq_venda.nextval, '0', TIMESTAMP '2023-03-16 14:30:00', 2, 7, 'b9e19222-412e-43eb-8f37-46ab322e77d3');
 
 INSERT INTO AVIACAO.VENDA  (id_venda, status, data, id_companhia, id_comprador, codigo)
-	VALUES (AVIACAO.seq_venda.nextval, 1 , TIMESTAMP '2023-03-16 14:30:00', 3, 8, 'd744dfb4-6f57-45fa-96a3-e8c3349b71ec');
+	VALUES (AVIACAO.seq_venda.nextval, '0' , TIMESTAMP '2023-03-16 14:30:00', 3, 8, 'd744dfb4-6f57-45fa-96a3-e8c3349b71ec');
 
 INSERT INTO AVIACAO.VENDA  (id_venda, status, data, id_companhia, id_comprador, codigo)
-	VALUES (AVIACAO.seq_venda.nextval, 1 , TIMESTAMP '2023-03-16 14:30:00', 4, 9, '4e5e9c7d-4b72-4dc4-a984-22bae61473fd');
+	VALUES (AVIACAO.seq_venda.nextval, '0' , TIMESTAMP '2023-03-16 14:30:00', 4, 9, '4e5e9c7d-4b72-4dc4-a984-22bae61473fd');
 
 INSERT INTO AVIACAO.VENDA  (id_venda, status, data, id_companhia, id_comprador, codigo)
-	VALUES (AVIACAO.seq_venda.nextval, 1 , TIMESTAMP '2023-03-16 14:30:00', 5, 10, 'e0470f31-f930-488c-b6e9-4f15236fabb7');
+	VALUES (AVIACAO.seq_venda.nextval, '0' , TIMESTAMP '2023-03-16 14:30:00', 5, 10, 'e0470f31-f930-488c-b6e9-4f15236fabb7');
 
 -- passagens --
 -- [0] - false
 -- [1] - true
 
-INSERT INTO AVIACAO.PASSAGEM (id_passagem, codigo, data_partida, data_chegada, disponivel, valor, id_trecho, id_venda)
+INSERT INTO AVIACAO.PASSAGEM (id_passagem, codigo, data_partida, data_chegada, status, valor, id_companhia, id_trecho, id_venda)
 	VALUES (AVIACAO.seq_passagem.nextval, 'd1a1b20c-a2be-4995-983b-bc386dcee06a',
 		TO_TIMESTAMP('17-02-2023 16:18','dd-MM-yyyyHH24:MI'),
-        TO_TIMESTAMP('18-02-2023 16:18','dd-MM-yyyyHH24:MI'), 0, 200.9, 1, 1);
-	
-INSERT INTO AVIACAO.PASSAGEM (id_passagem, codigo, data_partida, data_chegada, disponivel, valor, id_trecho, id_venda)
+        TO_TIMESTAMP('18-02-2023 16:18','dd-MM-yyyyHH24:MI'), '3', 200.9, 1, 1, 1);
+
+INSERT INTO AVIACAO.PASSAGEM (id_passagem, codigo, data_partida, data_chegada, status, valor, id_companhia, id_trecho, id_venda)
 	VALUES (AVIACAO.seq_passagem.nextval, 'f871a744-7165-4b38-9710-694208b98970',
         TO_TIMESTAMP('17-02-2023 16:18','dd-MM-yyyyHH24:MI'),
-	    TO_TIMESTAMP('18-02-2023 16:18','dd-MM-yyyyHH24:MI'), 0, 650, 2, 2);
-	
-INSERT INTO AVIACAO.PASSAGEM (id_passagem, codigo, data_partida, data_chegada, disponivel, valor, id_trecho, id_venda)
+	    TO_TIMESTAMP('18-02-2023 16:18','dd-MM-yyyyHH24:MI'), '3', 650, 2, 2, 2);
+
+INSERT INTO AVIACAO.PASSAGEM (id_passagem, codigo, data_partida, data_chegada, status, valor, id_companhia, id_trecho, id_venda)
 	VALUES (AVIACAO.seq_passagem.nextval, 'ca3b7258-1ef2-450b-98e8-79f5e3ccc3ee',
         TO_TIMESTAMP('17-02-2023 16:18','dd-MM-yyyyHH24:MI'),
-	    TO_TIMESTAMP('18-02-2023 16:18','dd-MM-yyyyHH24:MI'), 0, 790.5, 3, 3);
-	
-INSERT INTO AVIACAO.PASSAGEM (id_passagem, codigo, data_partida, data_chegada, disponivel, valor, id_trecho, id_venda)
+	    TO_TIMESTAMP('18-02-2023 16:18','dd-MM-yyyyHH24:MI'), '3', 790.5, 3, 3, 3);
+
+INSERT INTO AVIACAO.PASSAGEM (id_passagem, codigo, data_partida, data_chegada, status, valor, id_companhia, id_trecho, id_venda)
 	VALUES (AVIACAO.seq_passagem.nextval, '8c8fea7a-6c96-45c4-b4cc-a6873cb0b791',
         TO_TIMESTAMP('17-02-2023 16:18','dd-MM-yyyyHH24:MI'),
-	    TO_TIMESTAMP('18-02-2023 16:18','dd-MM-yyyyHH24:MI'), 0, 364.2, 4, 4);
-	
-INSERT INTO AVIACAO.PASSAGEM (id_passagem, codigo, data_partida, data_chegada, disponivel, valor, id_trecho, id_venda)
+	    TO_TIMESTAMP('18-02-2023 16:18','dd-MM-yyyyHH24:MI'), '3', 364.2, 4, 4, 4);
+
+INSERT INTO AVIACAO.PASSAGEM (id_passagem, codigo, data_partida, data_chegada, status, valor, id_companhia, id_trecho, id_venda)
 	VALUES (AVIACAO.seq_passagem.nextval, 'd440f0fd-6c7d-4b66-9a34-6193abe3ee1d',
 	    TO_TIMESTAMP('17-02-2023 16:18','dd-MM-yyyyHH24:MI'),
-	    TO_TIMESTAMP('18-02-2023 16:18','dd-MM-yyyyHH24:MI'), 0, 861.8, 5, 5);
-	
+	    TO_TIMESTAMP('18-02-2023 16:18','dd-MM-yyyyHH24:MI'), '3', 861.8, 5, 5, 5);
+
+
 
