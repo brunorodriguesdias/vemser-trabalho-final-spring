8cbd1e8ab21c475b30549abe72d0cc462987ff5d
Bruno <brunorodriguesdias@gmail.com>
2023-03-24 11:09:10 -0300

    wip

--- src/main/java/br/com/dbc/javamosdecolar/service/UsuarioService.java
+++ src/main/java/br/com/dbc/javamosdecolar/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package br.com.dbc.javamosdecolar.service;
 
+import br.com.dbc.javamosdecolar.entity.UsuarioEntity;
 import br.com.dbc.javamosdecolar.exception.RegraDeNegocioException;
 import br.com.dbc.javamosdecolar.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,4 +21,34 @@ public class UsuarioService {
             throw new RegraDeNegocioException("Este login já está cadastrado!");
         }
     }
+
+//    public UsuarioEntity autenticar(LoginDTO loginDTO) throws RegraDeNegocioException {
+//        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+//                new UsernamePasswordAuthenticationToken(
+//                        loginDTO.getLogin(),
+//                        loginDTO.getSenha());
+//
+//        Authentication authentication;
+//        try {
+//            authentication =
+//                    authenticaticationManager.authenticate(usernamePasswordAuthenticationToken);
+//        } catch (BadCredentialsException ex) {
+//            throw new RegraDeNegocioException("Usuario não encontrado");
+//        }
+//
+//        Object principal = authentication.getPrincipal();
+//        UsuarioEntity usuarioEntity = (UsuarioEntity) principal;
+//
+//        return usuarioEntity;
+//    }
+//
+//    public Integer getIdLoggedUser() {
+//        return Integer.parseInt(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
+//    }
+//
+//    public LoginDTO getLoggedUser() throws RegraDeNegocioException {
+//        UsuarioEntity usuarioEntity = findByUsuario(getIdLoggedUser())
+//                .orElseThrow(() -> new RegraDeNegocioException("Usuário não encontrado!"));
+//        return objectMapper.convertValue(usuarioEntity, LoginDTO.class);
+//    }
 }
