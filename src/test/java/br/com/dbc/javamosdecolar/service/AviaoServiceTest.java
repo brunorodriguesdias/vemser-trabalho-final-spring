5ee9390a30e6e7afc64b3ec7c27abbf083b40fab
Bruno <brunorodriguesdias@gmail.com>
2023-03-29 19:41:53 -0300

    wip

---
+++ src/test/java/br/com/dbc/javamosdecolar/service/AviaoServiceTest.java
@@ -0,0 +1,125 @@
+package br.com.dbc.javamosdecolar.service;
+
+import br.com.dbc.javamosdecolar.dto.in.AviaoCreateDTO;
+import br.com.dbc.javamosdecolar.dto.outs.AviaoDTO;
+import br.com.dbc.javamosdecolar.dto.outs.PageDTO;
+import br.com.dbc.javamosdecolar.entity.AviaoEntity;
+import br.com.dbc.javamosdecolar.entity.CompanhiaEntity;
+import br.com.dbc.javamosdecolar.entity.enums.TipoUsuario;
+import br.com.dbc.javamosdecolar.exception.RegraDeNegocioException;
+import br.com.dbc.javamosdecolar.repository.AviaoRepository;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AviaoServiceTest {
+
+    @InjectMocks
+    private AviaoService aviaoService;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Mock
+    private AviaoRepository aviaoRepository;
+
+    @Mock
+    private CompanhiaService companhiaService;
+
+    @Before
+    public void init() {
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        ReflectionTestUtils.setField(aviaoService, "objectMapper", objectMapper);
+    }
+
+    @Test
+    public void deveListarComSucesso() {
+        List<AviaoDTO> listaDTO = List.of(getAviaoDTOEntityMock(), getAviaoDTOEntityMock(), getAviaoDTOEntityMock());
+        Page<AviaoDTO> listaPaginada = new PageImpl<>(listaDTO);
+        when(aviaoRepository.getAllAviaoWithCompanhia(Mockito.any(Pageable.class))).thenReturn(listaPaginada);
+
+        PageDTO<AviaoDTO> list = aviaoService.getAll(0, 3);
+
+        assertNotNull(list);
+        assertEquals(listaPaginada.getTotalElements(), list.getTotalElementos());
+        assertEquals(listaPaginada.getTotalPages(), list.getQuantidadePaginas());
+        assertEquals(0, list.getPagina());
+        assertEquals(listaPaginada.getSize(), list.getTamanho());
+        assertEquals(listaPaginada.getContent(), list.getElementos());
+    }
+
+    @Test
+    public void deveCriarComSucesso() throws RegraDeNegocioException {
+        AviaoCreateDTO aviaoCreateDTO = getAviaoDTOEntityMock();
+        AviaoEntity aviaoEntity = getAviaoEntityMock();
+
+        when(aviaoRepository.existsByCodigoAviao(Mockito.anyString())).thenReturn(false);
+        when(companhiaService.getCompanhiaSemId()).thenReturn(getCompanhiaEntityMock());
+
+        AviaoDTO aviaoRetornado = aviaoService.create(aviaoCreateDTO);
+
+        assertNotNull(aviaoRetornado);
+        assertEquals(aviaoRetornado.getCodigoAviao(), aviaoEntity.getCodigoAviao());
+
+    }
+
+    @NotNull
+    private static AviaoDTO getAviaoDTOEntityMock() {
+        AviaoDTO aviaoDTOMockadaDoBanco = new AviaoDTO();
+        aviaoDTOMockadaDoBanco.setIdAviao(1);
+        aviaoDTOMockadaDoBanco.setIdCompanhia(1);
+        aviaoDTOMockadaDoBanco.setCodigoAviao("PT-ZXE");
+        aviaoDTOMockadaDoBanco.setCapacidade(200);
+        aviaoDTOMockadaDoBanco.setUltimaManutencao(LocalDate.of(2000, 9, 8));
+        aviaoDTOMockadaDoBanco.setAtivo(true);
+        return aviaoDTOMockadaDoBanco;
+    }
+
+    @NotNull
+    private static AviaoEntity getAviaoEntityMock() {
+        AviaoEntity aviaoMockadaDoBanco = new AviaoEntity();
+        aviaoMockadaDoBanco.setIdAviao(1);
+        aviaoMockadaDoBanco.setIdCompanhia(1);
+        aviaoMockadaDoBanco.setCodigoAviao("PT-ZXE");
+        aviaoMockadaDoBanco.setCapacidade(200);
+        aviaoMockadaDoBanco.setUltimaManutencao(LocalDate.of(2000, 9, 8));
+        aviaoMockadaDoBanco.setAtivo(true);
+        return aviaoMockadaDoBanco;
+    }
+
+    @NotNull
+    private static CompanhiaEntity getCompanhiaEntityMock() {
+        CompanhiaEntity companhiaMockadaDoBanco = new CompanhiaEntity();
+        companhiaMockadaDoBanco.setIdUsuario(1);
+        companhiaMockadaDoBanco.setTipoUsuario(TipoUsuario.COMPANHIA);
+        companhiaMockadaDoBanco.setNome("tam aviao");
+        companhiaMockadaDoBanco.setNomeFantasia("TAM");
+        companhiaMockadaDoBanco.setSenha("1234");
+        companhiaMockadaDoBanco.setCnpj("51543712000198");
+        companhiaMockadaDoBanco.setAtivo(true);
+        return companhiaMockadaDoBanco;
+    }
+}
