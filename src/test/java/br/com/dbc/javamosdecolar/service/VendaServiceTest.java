77e2e24f2257fce66f624da3540c0321d64aacc2
Bruno <brunorodriguesdias@gmail.com>
2023-03-30 20:58:26 -0300

    Finalizando os testes da classe VendaService

--- src/test/java/br/com/dbc/javamosdecolar/service/VendaServiceTest.java
+++ src/test/java/br/com/dbc/javamosdecolar/service/VendaServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.dbc.javamosdecolar.service;
 
 import br.com.dbc.javamosdecolar.dto.in.VendaCreateDTO;
+import br.com.dbc.javamosdecolar.dto.outs.PageDTO;
 import br.com.dbc.javamosdecolar.dto.outs.UsuarioDTO;
 import br.com.dbc.javamosdecolar.dto.outs.VendaDTO;
 import br.com.dbc.javamosdecolar.entity.*;
@@ -80,6 +81,19 @@ public class VendaServiceTest {
         assertEquals(compradorEntity.getNome(), vendaDTO.getComprador());
         assertEquals(companhiaEntity.getNome(), vendaDTO.getCompanhia());
     }
+
+    @Test(expected = RegraDeNegocioException.class)
+    public void deveTestarPassagemIndisponivel() throws RegraDeNegocioException {
+        PassagemEntity passagemEntity = getPassagemEntityMock();
+        VendaEntity vendaEntity = getVendaEntityMock();
+        CompradorEntity compradorEntity = getCompradorEntityMock();
+        passagemEntity.setStatus(Status.CANCELADO);
+
+        when(compradorService.getLoggedComprador()).thenReturn(compradorEntity);
+        when(passagemService.getPassagem(anyInt())).thenReturn(passagemEntity);
+
+        VendaDTO vendaDTO = vendaService.create(getVendaCreateDTOMock());
+    }
     @Test
     public void deveDeletarComSucesso() throws RegraDeNegocioException {
         VendaEntity vendaEntity = getVendaEntityMock();
@@ -110,12 +124,141 @@ public class VendaServiceTest {
         vendaService.delete(vendaEntity.getIdVenda());
     }
 
-//    @Test
-//    public void deveListarComprasCompradorComSucesso() {
-//        List<VendaDTO> comprasDTO = List.of(getVendaDTOMock(), getVendaDTOMock(), getVendaDTOMock());
-//        Page<VendaDTO> comprasPaginadas = new PageImpl<>(comprasDTO);
-//
-//    }
+    @Test(expected = RegraDeNegocioException.class)
+    public void deveTestarVendaCancelada() throws RegraDeNegocioException {
+        VendaEntity vendaEntity = getVendaEntityMock();
+        vendaEntity.setStatus(Status.CANCELADO);
+
+        when(vendaRepository.findById(anyInt())).thenReturn(Optional.of(vendaEntity));
+
+        vendaService.delete(vendaEntity.getIdVenda());
+    }
+
+    @Test
+    public void deveListarComprasComprador() throws RegraDeNegocioException {
+        CompradorEntity compradorEntity = getCompradorEntityMock();
+        UsuarioEntity usuarioEntity = getUsuarioEntityMock();
+        Integer pagina = 0;
+        Integer tamanho = 3;
+
+        List<VendaDTO> comprasDTO = List.of(getVendaDTOMock(), getVendaDTOMock(), getVendaDTOMock());
+        Page<VendaDTO> comprasPaginadas = new PageImpl<>(comprasDTO);
+
+        when(compradorService.getCompradorComId(anyInt())).thenReturn(compradorEntity);
+        when(usuarioService.getIdLoggedUser()).thenReturn(usuarioEntity.getIdUsuario());
+        when(vendaRepository.findAllByIdComprador(anyInt(), any())).thenReturn(comprasPaginadas);
+
+        PageDTO<VendaDTO> comprasPaginadasDTO = vendaService.getHistoricoComprasComprador(compradorEntity.getIdUsuario(), pagina, tamanho);
+
+        assertNotNull(comprasPaginadasDTO);
+        assertEquals(pagina, comprasPaginadasDTO.getPagina());
+        assertEquals(tamanho, comprasPaginadasDTO.getTamanho());
+        assertEquals(comprasPaginadas.getTotalElements(), comprasPaginadasDTO.getTotalElementos());
+        assertEquals(comprasPaginadas.getTotalPages(), comprasPaginadasDTO.getQuantidadePaginas());
+        assertEquals(comprasPaginadas.getContent(), comprasPaginadasDTO.getElementos());
+    }
+
+    @Test(expected = RegraDeNegocioException.class)
+    public void deveTestarPermissaoNegadaComprador() throws RegraDeNegocioException {
+        CompradorEntity compradorEntity = getCompradorEntityMock();
+        UsuarioEntity usuarioEntity = getUsuarioEntityMock();
+        UsuarioDTO usuarioDTO = getUsuarioDTOMock();
+        Integer pagina = 0;
+        Integer tamanho = 3;
+
+        List<VendaDTO> comprasDTO = List.of(getVendaDTOMock(), getVendaDTOMock(), getVendaDTOMock());
+        Page<VendaDTO> comprasPaginadas = new PageImpl<>(comprasDTO);
+
+
+        when(compradorService.getCompradorComId(anyInt())).thenReturn(compradorEntity);
+        when(usuarioService.getIdLoggedUser()).thenReturn(usuarioEntity.getIdUsuario());
+        when(usuarioService.getLoggedUser()).thenReturn(usuarioDTO);
+
+        PageDTO<VendaDTO> comprasPaginadasDTO = vendaService.getHistoricoComprasComprador(32, pagina, tamanho);
+    }
+
+    @Test
+    public void deveListarVendasCompanhia() throws RegraDeNegocioException {
+        CompanhiaEntity companhiaEntity = getCompanhiaEntityMock();
+        UsuarioEntity usuarioEntity = getUsuarioEntityMock();
+        usuarioEntity.setIdUsuario(1);
+        UsuarioDTO usuarioDTO = getUsuarioDTOMock();
+        Integer pagina = 0;
+        Integer tamanho = 3;
+
+        List<VendaDTO> vendasDTO = List.of(getVendaDTOMock(), getVendaDTOMock(), getVendaDTOMock());
+        Page<VendaDTO> vendasPaginadas = new PageImpl<>(vendasDTO);
+
+        when(companhiaService.getCompanhiaComId(anyInt())).thenReturn(companhiaEntity);
+        when(usuarioService.getIdLoggedUser()).thenReturn(usuarioEntity.getIdUsuario());
+        when(vendaRepository.findAllByIdCompanhia(anyInt(), any())).thenReturn(vendasPaginadas);
+
+        PageDTO<VendaDTO> vendasPaginadasDTO = vendaService.getHistoricoVendasCompanhia(companhiaEntity.getIdUsuario(), pagina, tamanho);
+
+        assertNotNull(vendasPaginadasDTO);
+        assertEquals(pagina, vendasPaginadasDTO.getPagina());
+        assertEquals(tamanho, vendasPaginadasDTO.getTamanho());
+        assertEquals(vendasPaginadas.getTotalElements(), vendasPaginadasDTO.getTotalElementos());
+        assertEquals(vendasPaginadas.getTotalPages(), vendasPaginadasDTO.getQuantidadePaginas());
+        assertEquals(vendasPaginadas.getContent(), vendasPaginadasDTO.getElementos());
+    }
+
+    @Test(expected = RegraDeNegocioException.class)
+    public void deveTestarPermissaoNegadaCompanhia() throws RegraDeNegocioException {
+        CompanhiaEntity companhiaEntity = getCompanhiaEntityMock();
+        UsuarioEntity usuarioEntity = getUsuarioEntityMock();
+        UsuarioDTO usuarioDTO = getUsuarioDTOMock();
+        Integer pagina = 0;
+        Integer tamanho = 3;
+
+        List<VendaDTO> vendasDTO = List.of(getVendaDTOMock(), getVendaDTOMock(), getVendaDTOMock());
+        Page<VendaDTO> vendasPaginadas = new PageImpl<>(vendasDTO);
+
+
+        when(companhiaService.getCompanhiaComId(anyInt())).thenReturn(companhiaEntity);
+        when(usuarioService.getIdLoggedUser()).thenReturn(usuarioEntity.getIdUsuario());
+        when(usuarioService.getLoggedUser()).thenReturn(usuarioDTO);
+
+        PageDTO<VendaDTO> vendasPaginadasDTO = vendaService.getHistoricoVendasCompanhia(32, pagina, tamanho);
+    }
+
+    @Test
+    public void deveTestarVendasBetween() {
+        List<VendaDTO> vendasDTO = List.of(getVendaDTOMock(), getVendaDTOMock(), getVendaDTOMock());
+        Page<VendaDTO> vendasPaginadas = new PageImpl<>(vendasDTO);
+        LocalDateTime dataInicio = LocalDateTime.parse("2021-10-10T16:11:26.2");
+        LocalDateTime dataFim = LocalDateTime.parse("2024-10-10T16:11:26.2");
+        Integer pagina = 0;
+        Integer tamanho = 3;
+
+        when(vendaRepository.findAllByDataBetween(any(), any(), any())).thenReturn(vendasPaginadas);
+
+        PageDTO<VendaDTO> vendasBetween = vendaService.getVendasBetween(dataInicio, dataFim, pagina, tamanho);
+
+        assertNotNull(vendasBetween);
+        assertEquals(pagina, vendasBetween.getPagina());
+        assertEquals(tamanho, vendasBetween.getTamanho());
+        assertEquals(vendasPaginadas.getTotalElements(), vendasBetween.getTotalElementos());
+        assertEquals(vendasPaginadas.getTotalPages(), vendasBetween.getQuantidadePaginas());
+        assertEquals(vendasPaginadas.getContent(), vendasBetween.getElementos());
+    }
+
+    @Test
+    public void deveTestarListaPaginada() {
+        List<VendaDTO> vendasDTO = List.of(getVendaDTOMock(), getVendaDTOMock(), getVendaDTOMock());
+        Page<VendaDTO> vendasPaginadas = new PageImpl<>(vendasDTO);
+        Integer pagina = 0;
+        Integer tamanho = 3;
+
+        PageDTO<VendaDTO> vendasPaginadasDTO = vendaService.listaPaginada(vendasPaginadas,pagina, tamanho);
+
+        assertNotNull(vendasPaginadasDTO);
+        assertEquals(pagina, vendasPaginadasDTO.getPagina());
+        assertEquals(tamanho, vendasPaginadasDTO.getTamanho());
+        assertEquals(vendasPaginadas.getTotalElements(), vendasPaginadasDTO.getTotalElementos());
+        assertEquals(vendasPaginadas.getTotalPages(), vendasPaginadasDTO.getQuantidadePaginas());
+        assertEquals(vendasPaginadas.getContent(), vendasPaginadasDTO.getElementos());
+    }
 
     private static VendaCreateDTO getVendaCreateDTOMock() {
         VendaCreateDTO vendaCreateDTO = new VendaCreateDTO();
@@ -184,5 +327,11 @@ public class VendaServiceTest {
         UsuarioDTO usuarioDTO = new UsuarioDTO();
         usuarioDTO.setIdUsuario(2);
         return usuarioDTO;
+    }
+
+    private static UsuarioEntity getUsuarioEntityMock() {
+        UsuarioEntity usuarioEntity = new UsuarioEntity();
+        usuarioEntity.setIdUsuario(2);
+        return usuarioEntity;
     }
 }
