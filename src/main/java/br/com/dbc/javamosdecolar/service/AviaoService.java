4b3672b0dbfa844a537377803065cee43e59f848
Gabriel-Gomes-Meira <gabrielgomesm.contato@gmail.com>
2023-03-31 15:17:30 -0300

    Aviao Create

--- src/main/java/br/com/dbc/javamosdecolar/service/AviaoService.java
+++ src/main/java/br/com/dbc/javamosdecolar/service/AviaoService.java
@@ -3,8 +3,10 @@ package br.com.dbc.javamosdecolar.service;
 import br.com.dbc.javamosdecolar.dto.in.AviaoCreateDTO;
 import br.com.dbc.javamosdecolar.dto.outs.AviaoDTO;
 import br.com.dbc.javamosdecolar.dto.outs.PageDTO;
+import br.com.dbc.javamosdecolar.dto.outs.UsuarioDTO;
 import br.com.dbc.javamosdecolar.entity.AviaoEntity;
 import br.com.dbc.javamosdecolar.entity.CompanhiaEntity;
+import br.com.dbc.javamosdecolar.entity.enums.TipoUsuario;
 import br.com.dbc.javamosdecolar.exception.RegraDeNegocioException;
 import br.com.dbc.javamosdecolar.repository.AviaoRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,6 +25,7 @@ public class AviaoService {
     private final AviaoRepository aviaoRepository;
     private final ObjectMapper objectMapper;
     private final CompanhiaService companhiaService;
+    private final UsuarioService usuarioService;
 
     public PageDTO<AviaoDTO> getAll(Integer pagina, Integer tamanho) {
         Pageable solcitacaoPagina = PageRequest.of(pagina, tamanho);
@@ -41,16 +44,24 @@ public class AviaoService {
             throw new RegraDeNegocioException("Já existe avião com esse código!");
         }
 
+
         // RECUPERANDO COMPANHIA
-        CompanhiaEntity companhia = companhiaService.getCompanhiaSemId();
+        UsuarioDTO loggedUser = usuarioService.getLoggedUser();
+        CompanhiaEntity companhiaProprietaria;
+        if(loggedUser.getTipoUsuario() == TipoUsuario.ADMIN) {
+            companhiaProprietaria = companhiaService.getCompanhiaComId(aviaoCreateDTO.getIdCompanhia());
+        } else {
+            companhiaProprietaria = companhiaService.getCompanhiaSemId();
+        }
 
         AviaoEntity aviao = objectMapper.convertValue(aviaoCreateDTO, AviaoEntity.class);
         aviao.setAtivo(true);
+        aviao.setIdCompanhia(companhiaProprietaria.getIdUsuario());
 
         // SALVANDO REGISTRO E PREPARANDO RETORNO
         AviaoDTO aviaoDTO = objectMapper.convertValue(aviaoRepository.save(aviao)
                                                 , AviaoDTO.class);
-        aviaoDTO.setNomeCompanhia(companhia.getNomeFantasia());
+        aviaoDTO.setNomeCompanhia(companhiaProprietaria.getNomeFantasia());
 
         return aviaoDTO;
     }
