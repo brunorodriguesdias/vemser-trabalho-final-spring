9a0ae585d269344d55a995e4555216a436baac67
Gabriel-Gomes-Meira <gabrielgomesm.contato@gmail.com>
2023-04-05 16:05:16 -0300

    KafkaProducerConfig

---
+++ src/main/java/br/com/dbc/javamosdecolar/service/ProducerService.java
@@ -0,0 +1,43 @@
+package br.com.dbc.javamosdecolar.service;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProducerService {
+    @Value(value = "${kafka.topic}")
+    private String topic;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void enviarEmail(String mensagem) {
+        MessageBuilder<String> stringMessageBuilder = MessageBuilder.withPayload(mensagem)
+                .setHeader(KafkaHeaders.TOPIC, topic)
+                .setHeader(KafkaHeaders.MESSAGE_KEY, UUID.randomUUID().toString());
+
+        ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(stringMessageBuilder.build());
+        future.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onSuccess(SendResult result) {
+                log.info(" Log enviado para o kafka com o texto: {} ", mensagem);
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error(" Erro ao publicar duvida no kafka com a mensagem: {}", mensagem, ex);
+            }
+        });
+    }
+}
