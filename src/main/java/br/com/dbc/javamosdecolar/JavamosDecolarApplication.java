0d69372e13b21f1fa1d05346ce967ff07450dd56
Gabriel-Gomes-Meira <gabrielgomesm.contato@gmail.com>
2023-04-05 16:03:05 -0300

    Scheduling wip

--- src/main/java/br/com/dbc/javamosdecolar/JavamosDecolarApplication.java
+++ src/main/java/br/com/dbc/javamosdecolar/JavamosDecolarApplication.java
@@ -2,8 +2,10 @@ package br.com.dbc.javamosdecolar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class JavamosDecolarApplication {
 
 	public static void main(String[] args) {
